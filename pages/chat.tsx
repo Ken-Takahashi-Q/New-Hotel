c6f1a5172bb598669d566b1177bdff8f915529f4
Yathip Kenichi Takahashi <ken.yathip@gmail.com>
2023-03-01 16:43:46 +0700

    finished 1 side chat

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -22,6 +22,7 @@ const database = firebase.database();
 const MessageSender = () => {
 	const [message, setMessage] = useState("");
 	const [error, setError] = useState("");
+	const [messageList, setMessageList] = useState([]);
 
 	const handleMessageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
 		setMessage(event.target.value);
@@ -31,17 +32,52 @@ const MessageSender = () => {
 	const handleMessageSend = () => {
 		const messagesRef = database.ref("messages");
 		messagesRef.push({
-		message: message.trim()
+			message: message.trim()
 		});
 		setMessage("");
 	};
 
+	useEffect(() => {
+		const messagesRef = database.ref("messages");
+		
+		messagesRef.on("value", (snapshot) => {
+			const messages = snapshot.val();
+			const messageArray = [];
+			for (let id in messages) {
+				messageArray.push({ id, ...messages[id] });
+			}
+			setMessageList(messageArray);
+		});
+	}, [database]);
+
 	return (
 		<div className="flex absolute justify-between top-0 bg-gray-700 h-screen w-full">
+			{/* Message list */}
+			<div className="flex flex-col justify-start h-[90%] w-full p-4 overflow-y-scroll">
+				<div className="mt-12"></div>
 
+				{messageList.map((message, index) => (
+					<div
+						key={message.key}
+						className="flex flex-col justify-center ml-4 mb-3 px-4 py-2 text-white bg-[#326BFF] border border-solid border-[#979FA7] rounded-xl"
+						style={{ maxWidth: "max-content" }}
+					>
+						<div className="whitespace-pre-wrap break-words">
+							{message.message}
+						</div>
+					</div>
+				))}
+			</div>
+
+			{/* <div className='flex flex-col absolute top-16 left-0 justify-center h-12 ml-4 p-4 text-white bg-[#326BFF] border border-solid border-[#979FA7] rounded-lg'>
+				Message
+			</div>
+			<div className='flex flex-col absolute top-32 left-0 justify-center h-12 ml-4 p-4 text-white bg-[#326BFF] border border-solid border-[#979FA7] rounded-lg'>
+				Messageaaaaaaaaaaa
+			</div> */}
 		
 			{/* Message box */}
-			<div className='flex absolute justify-between bottom-0 h-16 w-full p-2 border-t border-solid border-[#979FA7]'>
+			<div className='flex absolute justify-between bottom-0 h-[10%]  w-full p-2 border-t border-solid border-[#979FA7]'>
 				<input
 					className='h-full mx-4 px-4 w-full border border-solid border-[#979FA7] rounded-lg text-base'
 					placeholder='Text a message'
